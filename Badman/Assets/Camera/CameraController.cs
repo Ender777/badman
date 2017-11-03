a7c499f5c8a41330ac4774e1911067c8d0153107
Brandon Ernst <brandonernst@Brandons-MacBook-Pro-2.local>
2017-11-02 21:43:08 -0700

    basic platformer controls

---
+++ Badman/Assets/Camera/CameraController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	public Player player;
+	public float delay = 0.25f;
+	private List<Sample> playerHistory = new List<Sample>();
+	private class Sample {
+		public Vector3 position;
+		public float time;
+	}
+	
+	private void StoreSample() {
+		Vector3 playerXZ = player.transform.position;
+		playerXZ.y = 0.0f;
+		playerHistory.Add(
+			new Sample(){
+				position = playerXZ,
+				time = Time.time
+			}	
+		);
+		var earliestSample = Time.time - delay;
+		while(playerHistory.Count != 0 && playerHistory[0].time < earliestSample){
+			playerHistory.RemoveAt(0);
+		}
+	}
+	private Vector3 GetViewTarget(){
+		Vector3 sum = Vector3.zero;
+		for(int i = 0; i < playerHistory.Count; ++i){
+			sum += playerHistory[i].position;
+		}
+		return sum / (float)playerHistory.Count;
+	}
+	
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		StoreSample();
+		Vector3 viewTarget = GetViewTarget();
+		Vector3 cameraPos = transform.position;
+		cameraPos.x = viewTarget.x;
+		cameraPos.y = viewTarget.y;
+		transform.position = cameraPos;
+	}
+	
+}
--- Badman/Assets/Player/Player.cs
+++ Badman/Assets/Player/Player.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-	public float speed = 10.0f;	
-	// Use this for initialization
-	void Start () {
-		
-	}
+	public float speed = 10.0f;
+	public float range = 3.0f;
+
+	private float traveled = 0.0f;
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = transform.position + Vector3.up * speed * Time.deltaTime;
+		var h = Input.GetAxis("Horizontal");
+		traveled += Time.deltaTime * speed * h;
+		
+		transform.position = 
+			(Vector3.up * Mathf.Sin(Time.time * speed) * range) + // vertical
+			(Vector3.right * traveled);
+
 	}
 }
